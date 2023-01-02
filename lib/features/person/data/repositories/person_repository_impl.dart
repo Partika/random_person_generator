26c38a943d5cfabe24952b5e7bb260e99a0525ae
Partika <matheusorzechowski@gmail.com>
2023-01-02 14:23:16 -0300

    test: person repository impl

--- lib/features/person/data/repositories/person_repository_impl.dart
+++ lib/features/person/data/repositories/person_repository_impl.dart
@@ -1,10 +1,23 @@
 import 'package:dartz/dartz.dart';
+import 'package:tarefa_2/core/platform/network_info.dart';
+import 'package:tarefa_2/features/person/data/datasources/person_local_data_source.dart';
+import 'package:tarefa_2/features/person/data/datasources/person_remote_data_source.dart';
 
 import '../../../../core/error/failures.dart';
 import '../../domain/entities/person.dart';
 import '../../domain/repositories/person_repository.dart';
 
-class PersonRepositoryImplementation implements PersonRepository {
+class PersonRepositoryImpl implements PersonRepository {
+  final PersonRemoteDataSource remoteDataSource;
+  final PersonLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  PersonRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+
   @override
   void deletePerson() {
     // TODO: implement deleteRandomPerson

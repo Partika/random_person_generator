b11090db3942ab856e8448f3bed26ed10e34845f
Partika <matheusorzechowski@gmail.com>
2023-01-02 13:46:14 -0300

    chore: implementando person repository

---
+++ lib/features/person/data/repositories/person_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/person.dart';
+import '../../domain/repositories/person_repository.dart';
+
+class PersonRepositoryImplementation implements PersonRepository {
+  @override
+  void deleteRandomPerson() {
+    // TODO: implement deleteRandomPerson
+  }
+
+  @override
+  Future<Either<Failure, Person>> getRandomPerson() {
+    // TODO: implement getRandomPerson
+    throw UnimplementedError();
+  }
+}

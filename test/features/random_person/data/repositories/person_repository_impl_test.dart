bbf35032c60788190e1ce0e4e0ba883d412f67ef
Partika <matheusorzechowski@gmail.com>
2023-01-02 15:21:03 -0300

    fix: test

--- test/features/random_person/data/repositories/person_repository_impl_test.dart
+++ test/features/random_person/data/repositories/person_repository_impl_test.dart
@@ -35,27 +35,56 @@ void main() {
     },
   );
 
-  const tPersonModel = PersonModel(
-    nameFirst: 'Test NFirst',
-    nameLast: 'Test NLast',
-    locationStreet: 'Test LStreet',
-    locationNumber: 123,
-    locationCity: 'Test LCity',
-    locationState: 'Test LState',
-    locationCountry: 'Test LCountry',
-    email: 'Test Email',
-    dateOfBirth: 'Test DoB',
-    age: 123,
-    phone: 'Test Phone',
-    cell: 'Test Cell',
-    picture: 'Test Pic Link',
-    nat: 'Test Nat',
-  );
-  const Person tPerson = tPersonModel;
+  void runTestsOnline(Function body) {
+    group(
+      'device is online',
+      () {
+        setUp(
+          () {
+            when(mockNetworkInfo.isConected).thenAnswer((_) async => true);
+          },
+        );
+
+        body();
+      },
+    );
+  }
+
+  void runTestsOffline(Function body) {
+    group(
+      'device is offline',
+      () {
+        setUp(
+          () {
+            when(mockNetworkInfo.isConected).thenAnswer((_) async => false);
+          },
+        );
+
+        body();
+      },
+    );
+  }
 
   group(
     'getRandomPerson',
     () {
+      const tPersonModel = PersonModel(
+        nameFirst: 'Test NFirst',
+        nameLast: 'Test NLast',
+        locationStreet: 'Test LStreet',
+        locationNumber: 123,
+        locationCity: 'Test LCity',
+        locationState: 'Test LState',
+        locationCountry: 'Test LCountry',
+        email: 'Test Email',
+        dateOfBirth: 'Test DoB',
+        age: 123,
+        phone: 'Test Phone',
+        cell: 'Test Cell',
+        picture: 'Test Pic Link',
+        nat: 'Test Nat',
+      );
+      const Person tPerson = tPersonModel;
       test(
         'should check if the device is online',
         () async {
@@ -67,100 +96,85 @@ void main() {
           verify(mockNetworkInfo.isConected);
         },
       );
-    },
-  );
 
-  group(
-    'device is online',
-    () {
-      setUp(
+      runTestsOnline(
         () {
-          when(mockNetworkInfo.isConected).thenAnswer((_) async => true);
+          test(
+            'should return remote data when the call to remote data source is successful',
+            () async {
+              // arrange
+              when(mockRemoteDataSource.getRandomPerson())
+                  .thenAnswer((_) async => tPersonModel);
+              // act
+              final result = await repository.getRandomPerson();
+              // assert
+              verify(mockRemoteDataSource.getRandomPerson());
+              expect(result, equals(const Right(tPersonModel)));
+            },
+          );
+
+          test(
+            'should cache the data locally when the call to remote data source is successful',
+            () async {
+              // arrange
+              when(mockRemoteDataSource.getRandomPerson())
+                  .thenAnswer((_) async => tPersonModel);
+              // act
+              await repository.getRandomPerson();
+              // assert
+              verify(mockRemoteDataSource.getRandomPerson());
+              verify(mockLocalDataSource.cachePerson(tPersonModel));
+            },
+          );
+
+          test(
+            'should return server failure when the call to remote data source is unsuccessful',
+            () async {
+              // arrange
+              when(mockRemoteDataSource.getRandomPerson()).thenThrow(
+                  ServerException('Houve um problema com o servidor!'));
+              // act
+              final result = await repository.getRandomPerson();
+              // assert
+              verify(mockRemoteDataSource.getRandomPerson());
+              verifyZeroInteractions(mockLocalDataSource);
+              expect(result, equals(const Left(tPersonModel)));
+            },
+          );
         },
       );
 
-      test(
-        'should return remote data when the call to remote data source is successful',
-        () async {
-          // arrange
-          when(mockRemoteDataSource.getRandomPerson())
-              .thenAnswer((_) async => tPersonModel);
-          // act
-          final result = await repository.getRandomPerson();
-          // assert
-          verify(mockRemoteDataSource.getRandomPerson());
-          expect(result, equals(const Right(tPersonModel)));
-          verifyNoMoreInteractions(mockRemoteDataSource);
-        },
-      );
-
-      test(
-        'should cache the data locally when the call to remote data source is successful',
-        () async {
-          // arrange
-          when(mockRemoteDataSource.getRandomPerson())
-              .thenAnswer((_) async => tPersonModel);
-          // act
-          await repository.getRandomPerson();
-          // assert
-          verify(mockRemoteDataSource.getRandomPerson());
-          verify(mockLocalDataSource.cachePerson(tPersonModel));
-        },
-      );
-
-      test(
-        'should return server failure when the call to remote data source is unsuccessful',
-        () async {
-          // arrange
-          when(mockRemoteDataSource.getRandomPerson())
-              .thenThrow(ServerException('Houve um problema com o servidor!'));
-          // act
-          final result = await repository.getRandomPerson();
-          // assert
-          verify(mockRemoteDataSource.getRandomPerson());
-          verifyZeroInteractions(mockLocalDataSource);
-          expect(result, equals(const Left(tPersonModel)));
-        },
-      );
-    },
-  );
-
-  group(
-    'device is offline',
-    () {
-      setUp(
+      runTestsOffline(
         () {
-          when(mockNetworkInfo.isConected).thenAnswer((_) async => false);
-        },
-      );
+          test(
+            'should return last locally cached data when the cached data is present',
+            () async {
+              // arrange
+              when(mockLocalDataSource.getlastPerson())
+                  .thenAnswer((_) async => tPersonModel);
+              // act
+              final result = await repository.getRandomPerson();
+              // assert
+              verifyZeroInteractions(mockRemoteDataSource);
+              verify(mockLocalDataSource.getlastPerson());
+              expect(result, equals(const Right(tPerson)));
+            },
+          );
 
-      test(
-        'should return last locally cached data when the cached data is present',
-        () async {
-          // arrange
-          when(mockLocalDataSource.getlastPerson())
-              .thenAnswer((_) async => tPersonModel);
-          // act
-          final result = await repository.getRandomPerson();
-          // assert
-          verifyZeroInteractions(mockRemoteDataSource);
-          verify(mockLocalDataSource.getlastPerson());
-          expect(result, equals(const Right(tPerson)));
-        },
-      );
-
-      test(
-        'should return cache failure when there is no cached data is present',
-        () async {
-          // arrange
-          when(mockLocalDataSource.getlastPerson())
-              .thenThrow(CacheException('Cache vazia!'));
-          // act
-          final result = await repository.getRandomPerson();
-          // assert
-          verifyZeroInteractions(mockRemoteDataSource);
-          verify(mockLocalDataSource.getlastPerson());
-          expect(result, equals(Left(CacheFailure())));
+          test(
+            'should return cache failure when there is no cached data present',
+            () async {
+              // arrange
+              when(mockLocalDataSource.getlastPerson())
+                  .thenThrow(CacheException('Cache vazia!'));
+              // act
+              final result = await repository.getRandomPerson();
+              // assert
+              verifyZeroInteractions(mockRemoteDataSource);
+              verify(mockLocalDataSource.getlastPerson());
+              expect(result, equals(Left(CacheFailure())));
+            },
+          );
         },
       );
     },

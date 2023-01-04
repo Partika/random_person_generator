bb546f322a08912c34fcd09e6c4f41ec3cdaab97
Partika <matheusorzechowski@gmail.com>
2023-01-04 15:06:11 -0300

    test: initial bloc state

--- test/features/person/presentation/bloc/person_bloc_test.dart
+++ test/features/person/presentation/bloc/person_bloc_test.dart
@@ -1,6 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tarefa_2/features/person/domain/usecases/get_random_person.dart';
+import 'package:tarefa_2/features/person/presentation/bloc/person/person_bloc.dart';
+
+import 'person_bloc_test.mocks.dart';
 
 @GenerateMocks([GetRandomPerson])
-void main() {}
+void main() {
+  late PersonBloc bloc;
+  late MockGetRandomPerson mockGetRandomPerson;
+
+  setUp(() {
+    mockGetRandomPerson = MockGetRandomPerson();
+    bloc = PersonBloc(random: mockGetRandomPerson);
+  });
+
+  test('initial state should be EmptyState', () {
+    // assert
+    expect(bloc.state, equals(EmptyState()));
+  });
+
+  group('bloc group', () {
+    test('should do something', () {
+      // arrange
+      // act
+      // assert
+    });
+  });
+}

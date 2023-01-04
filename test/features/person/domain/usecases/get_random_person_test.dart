c37616f6784ddf8949f2b75dc1905d8c8e275665
Partika <matheusorzechowski@gmail.com>
2023-01-04 11:20:04 -0300

    fix: local data test

--- test/features/person/domain/usecases/get_random_person_test.dart
+++ test/features/person/domain/usecases/get_random_person_test.dart
@@ -1,5 +1,5 @@
-// import 'package:mockito/annotations.dart';
-// import 'package:tarefa_2/features/person/domain/repositories/person_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:tarefa_2/features/person/domain/repositories/person_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -8,7 +8,7 @@ import 'package:tarefa_2/features/person/domain/entities/person.dart';
 import 'package:tarefa_2/features/person/domain/usecases/get_random_person.dart';
 import './get_random_person_test.mocks.dart';
 
-// @GenerateMocks([PersonRepository])
+@GenerateMocks([PersonRepository])
 void main() {
   late GetRandomPerson usecase;
   late MockPersonRepository mockPersonRepository;

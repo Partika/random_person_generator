9d03374f1e356db07a0baf8da3dadea726591e79
Partika <matheusorzechowski@gmail.com>
2023-01-05 11:01:14 -0300

    test: get random person event

--- lib/features/person/presentation/bloc/person/person_bloc.dart
+++ lib/features/person/presentation/bloc/person/person_bloc.dart
@@ -1,10 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:tarefa_2/core/error/failures.dart';
+import 'package:tarefa_2/core/usecases/usecase.dart';
 import 'package:tarefa_2/features/person/domain/entities/person.dart';
 import 'package:tarefa_2/features/person/domain/usecases/get_random_person.dart';
 
 part 'person_event.dart';
 part 'person_state.dart';
+
+const String serverFailureMessage = 'Server Failure';
+const String cacheFailureMessage = 'Cache Failure';
 
 class PersonBloc extends Bloc<PersonEvent, PersonState> {
   final GetRandomPerson getRandomPerson;
@@ -12,8 +17,26 @@ class PersonBloc extends Bloc<PersonEvent, PersonState> {
   PersonBloc({required GetRandomPerson random})
       : getRandomPerson = random,
         super(EmptyState()) {
-    on<PersonEvent>((event, emit) {
-      // TODO: implement event handler
+    on<PersonEvent>((event, emit) async {
+      if (event is GetRandomPersonEvent) {
+        emit(LoadingState());
+        final failureOrPerson = await getRandomPerson(NoParams());
+        failureOrPerson.fold(
+          (failure) => emit(ErrorState(message: _mapFailureToMessage(failure))),
+          (person) => emit(LoadedState(person: person)),
+        );
+      }
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return 'Unexpected error';
+    }
   }
 }
--- test/features/person/presentation/bloc/person_bloc_test.dart
+++ test/features/person/presentation/bloc/person_bloc_test.dart
@@ -1,6 +1,10 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tarefa_2/core/error/failures.dart';
+import 'package:tarefa_2/core/usecases/usecase.dart';
+import 'package:tarefa_2/features/person/domain/entities/person.dart';
 import 'package:tarefa_2/features/person/domain/usecases/get_random_person.dart';
 import 'package:tarefa_2/features/person/presentation/bloc/person/person_bloc.dart';
 
@@ -16,16 +20,103 @@ void main() {
     bloc = PersonBloc(random: mockGetRandomPerson);
   });
 
+  blocTest(
+    'emits [EmptyState] when initial state.',
+    build: () => bloc,
+    act: (bloc) => bloc.add(MyEvent),
+    expect: () => const <SubjectState>[MyState],
+  );
+
   test('initial state should be EmptyState', () {
     // assert
     expect(bloc.state, equals(EmptyState()));
   });
 
-  group('bloc group', () {
-    test('should do something', () {
-      // arrange
-      // act
-      // assert
+  group('GetRandomPersonEvent', () {
+    const tPerson = Person(
+      nameFirst: 'nameFirst',
+      nameLast: 'nameLast',
+      locationStreet: 'locationStreet',
+      locationNumber: 123,
+      locationCity: 'locationCity',
+      locationState: 'locationState',
+      locationCountry: 'locationCountry',
+      email: 'email',
+      dateOfBirth: 'dateOfBirth',
+      age: 123,
+      phone: 'phone',
+      cell: 'cell',
+      picture: 'picture',
+      nat: 'nat',
+    );
+    test(
+      'should get data from the random use case',
+      () async {
+        // arrange
+        when(mockGetRandomPerson(any))
+            .thenAnswer((_) async => const Right(tPerson));
+        // act
+        bloc.add(GetRandomPersonEvent());
+        await untilCalled(mockGetRandomPerson(any));
+        // assert
+        verify(mockGetRandomPerson(NoParams()));
+      },
+    );
+
+    test(
+      'should emit [LoadingEvent, LoadedEvent] when data is gotten successfully',
+      () async {
+        // arrange
+        when(mockGetRandomPerson(any))
+            .thenAnswer((_) async => const Right(tPerson));
+        // assert later
+        final expected = [
+          EmptyState(),
+          LoadingState(),
+          const LoadedState(person: tPerson),
+        ];
+        expectLater(bloc.state, emitsInOrder(expected));
+        // act
+        bloc.add(GetRandomPersonEvent());
+      },
+    );
+
+    group('Failures', () {
+      test(
+        'should emit [LoadingEvent, ErrorEvent] when getting data fails',
+        () async {
+          // arrange
+          when(mockGetRandomPerson(any))
+              .thenAnswer((_) async => Left(ServerFailure()));
+          // assert later
+          final expected = [
+            EmptyState(),
+            LoadingState(),
+            const ErrorState(message: serverFailureMessage),
+          ];
+          expectLater(bloc.state, emitsInOrder(expected));
+          // act
+          bloc.add(GetRandomPersonEvent());
+        },
+      );
+
+      test(
+        'should emit [LoadingEvent, ErrorEvent] whit proper message for the error when getting data fails',
+        () async {
+          // arrange
+          when(mockGetRandomPerson(any))
+              .thenAnswer((_) async => Left(CacheFailure()));
+          // assert later
+          final expected = [
+            EmptyState(),
+            LoadingState(),
+            const ErrorState(message: cacheFailureMessage),
+          ];
+          expectLater(bloc.state, emitsInOrder(expected));
+          // act
+          bloc.add(GetRandomPersonEvent());
+        },
+      );
     });
   });
 }

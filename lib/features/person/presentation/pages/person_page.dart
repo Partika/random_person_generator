4e26ed11ff72087f27a5d9705888526f13718159
Partika <matheusorzechowski@gmail.com>
2023-01-10 11:41:29 -0300

    style: paddings

--- lib/features/person/presentation/pages/person_page.dart
+++ lib/features/person/presentation/pages/person_page.dart
@@ -63,103 +63,106 @@ class PersonBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width * 0.95,
       alignment: Alignment.topLeft,
       decoration: const BoxDecoration(
         color: Color(0xFFFFFDD4),
         borderRadius: BorderRadius.all(Radius.circular(10)),
         boxShadow: [BoxShadow(blurRadius: 10)],
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Text(
-            'NAME:',
-            style: fieldNameStyle,
-          ),
-          Text(
-            person.nameFirst + ' ' + person.nameLast,
-            style: fieldContentStyle,
-          ),
-          const Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Text>[
-                  const Text(
-                    'AGE:',
-                    style: fieldNameStyle,
-                  ),
-                  Text(
-                    '${person.age} YEARS',
-                    style: fieldContentStyle,
-                  ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.only(left: 100)),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Text>[
-                  const Text(
-                    'BIRTH DATE:',
-                    style: fieldNameStyle,
-                  ),
-                  Text(
-                    person.dateOfBirth,
-                    style: fieldContentStyle,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const Padding(padding: EdgeInsets.only(top: 10)),
-          const Text(
-            'EMAIL:',
-            style: fieldNameStyle,
-          ),
-          Text(
-            person.email,
-            style: fieldContentStyle,
-          ),
-          const Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Text>[
-                  const Text(
-                    'PHONE:',
-                    style: fieldNameStyle,
-                  ),
-                  Text(
-                    person.phone,
-                    style: fieldContentStyle,
-                  ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.only(left: 100)),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Text>[
-                  const Text(
-                    'CELL:',
-                    style: fieldNameStyle,
-                  ),
-                  Text(
-                    person.cell,
-                    style: fieldContentStyle,
-                  ),
-                ],
-              ),
-            ],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'NAME:',
+              style: fieldNameStyle,
+            ),
+            Text(
+              (person.nameFirst + ' ' + person.nameLast).toUpperCase(),
+              style: fieldContentStyle,
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            Row(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Text>[
+                    const Text(
+                      'AGE:',
+                      style: fieldNameStyle,
+                    ),
+                    Text(
+                      '${person.age} YEARS',
+                      style: fieldContentStyle,
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(left: 100)),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Text>[
+                    const Text(
+                      'BIRTH DATE:',
+                      style: fieldNameStyle,
+                    ),
+                    Text(
+                      person.dateOfBirth,
+                      style: fieldContentStyle,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            const Text(
+              'EMAIL:',
+              style: fieldNameStyle,
+            ),
+            Text(
+              person.email.toUpperCase(),
+              style: fieldContentStyle,
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            Row(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Text>[
+                    const Text(
+                      'PHONE:',
+                      style: fieldNameStyle,
+                    ),
+                    Text(
+                      person.phone,
+                      style: fieldContentStyle,
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(left: 100)),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Text>[
+                    const Text(
+                      'CELL:',
+                      style: fieldNameStyle,
+                    ),
+                    Text(
+                      person.cell,
+                      style: fieldContentStyle,
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -185,12 +188,14 @@ class PersonHeader extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(10)),
         boxShadow: [BoxShadow(blurRadius: 10)],
       ),
-      child: Column(
-        children: <Widget>[
-          const Padding(padding: EdgeInsets.only(top: 20)),
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            IconButton(
+              // TODO: tem que ir pra esquerda
               onPressed: () => Navigator.pop(context),
               icon: const Icon(
                 Icons.arrow_back,
@@ -198,49 +203,32 @@ class PersonHeader extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: SizedBox.fromSize(
-                  size: const Size.fromRadius(60),
-                  child: Image.network(person.picture),
-                ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: SizedBox.fromSize(
+                size: const Size.fromRadius(60),
+                child: Image.network(person.picture),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                person.username + ', ${person.age}',
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            Text(
+              person.username.toUpperCase() + ', ${person.age}',
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                person.email,
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+            Text(
+              person.email,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
